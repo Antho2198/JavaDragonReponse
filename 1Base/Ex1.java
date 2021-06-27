447da421de00873ff7c97c1dda88e469d93a6b2c
Anthony <anthony.dragone17@gmail.com>
2021-06-27 11:04:18 +0200

    2eme push
    modification effectuée sur la lecture robuste

--- 1Base/Ex1.java
+++ 1Base/Ex1.java
@@ -1,20 +1,19 @@
 import java.util.Scanner;
 
 public class Ex1 {
-    
+
     public static void main(String[] args) {
         Scanner clavier = new Scanner(System.in);
         System.out.println("Entrez votre nom");
-         String nom = clavier.nextLine();
+        String nom = clavier.nextLine();
         System.out.println("Entrez votre âge");
         int age = clavier.nextInt();
-        
 
-    if(age<=17){
-        System.out.println("Bonjour"+" "+nom+" "+"vous êtes mineur, allez jouer à Diablo.");
-    }else{
-        System.out.println("Bonjour"+"  "+nom+" "+"vous êtes majeur, allez jouer à Docteur Maboul ;)");
+        if (age <= 17) {
+            System.out.println("Bonjour" + " " + nom + " " + "vous êtes mineur, allez jouer à Diablo.");
+        } else {
+            System.out.println("Bonjour" + "  " + nom + " " + "vous êtes majeur, allez jouer à Docteur Maboul ;)");
+        }
     }
-    }    
 
-} 
+}
--- 1Base/Ex2.java
+++ 1Base/Ex2.java
@@ -1,24 +1,27 @@
 import java.util.Scanner;
 
+import jdk.internal.org.objectweb.asm.tree.MethodInsnNode;
+
 public class Ex2 {
-    
+
     public static void main(String[] args) {
         Scanner clavier = new Scanner(System.in);
         System.out.println("Entrez votre nom");
-         String nom = clavier.nextLine();
+        String nom = clavier.nextLine();
         System.out.println("Entrez votre âge");
         int age = clavier.nextInt();
         
-    if(age<0){
-    System.out.println("T'es pas né chef, tu me prends pour un jambon toi");
-        } else if(age<=17){
-        System.out.println("Bonjour"+" "+nom+" "+"vous êtes mineur, allez jouer à Diablo.");
-        } else if(age<=100){
-        System.out.println("Allé rentre bg");
-        } else if(age>100){
-        System.out.println("Bonjour"+" "+nom+", "+"vous êtes majeur mais sans doute mort à moins que vous soyez la Reine d'Anglettere ;)");
+            if(nom.equals("")){
+                System.out.println("Entre un nom chef");
+        } else if (age < 0) {
+            System.out.println("T'es pas né chef, tu me prends pour un jambon toi");
+        } else if (age <= 17) {
+            System.out.println("Bonjour" + " " + nom + " " + "vous êtes mineur, allez jouer à Diablo.");
+        } else if (age <= 100) {
+            System.out.println("Allé rentre bg");
+        } else if (age > 100) {
+            System.out.println("Bonjour" + " " + nom + ", "
+                    + "vous êtes majeur mais sans doute mort à moins que vous soyez la Reine d'Anglettere ;)");
         }
-        }
-        }
-    
-
+    }
+}
