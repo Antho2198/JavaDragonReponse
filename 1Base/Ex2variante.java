bcf60e19ec7a9cf38aee03c7c919c6e423fce37f
Anthony <anthony.dragone17@gmail.com>
2021-06-27 17:21:06 +0200

    ex2 variante

--- 1Base/Ex2variante.java
+++ 1Base/Ex2variante.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 
-public class Ex2 {
+public class Ex2variante {
 
     public static void main(String[] args) {
         Scanner clavier = new Scanner(System.in);
-
         System.out.println("Entrez votre nom");
         String nom = clavier.nextLine();
-
         System.out.println("Entrez votre âge");
         int age = clavier.nextInt();
 
-        if (nom.equals("")) {
+        if (nom.equals("") && (age > 120 ||  age <0 )) {
+            System.out.println("T'es bourré chef?");
+        } else if (nom.equals("")) {
             System.out.println("Entre un nom chef");
         } else if (age < 0) {
             System.out.println("T'es pas né chef, tu me prends pour un jambon toi");
@@ -19,7 +19,7 @@ public class Ex2 {
             System.out.println("Bonjour  " + nom + " vous êtes mineur, allez jouer à Diablo.");
         } else if (age <= 100) {
             System.out.println("Allé rentre bg");
-        } else if (age > 100) {
+        } else {
             System.out.println("Bonjour  " + nom + ", "
                     + "vous êtes majeur mais sans doute mort à moins que vous soyez la Reine d'Anglettere ;)");
         }
