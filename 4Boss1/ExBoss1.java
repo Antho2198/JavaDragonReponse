d4190b62dcb9fdba5bab529b6091b22718f9490f
Antho2198 <anthony.dragone17@gmail.com>
2021-07-03 20:40:29 +0200

    Boss 1

---
+++ 4Boss1/ExBoss1.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class ExBoss1 {
+    public static void main(String[] args) {
+        Scanner clavier = new Scanner(System.in);
+        System.out.println("Choisissez le niveau de difficulte entre 5 et 100");
+        while (!clavier.hasNextInt()) {
+            System.out.println("Entrez un entier");
+            clavier.next();
+        }
+        int niveau = clavier.nextInt();
+        while (niveau > 100 || niveau < 5) {
+            System.out.println("Entre 5 et 100 j'ai dit");
+            while (!clavier.hasNextInt()) {
+                System.out.println("Entrez un entier ");
+                clavier.next();
+            }
+            niveau = clavier.nextInt();
+        }
+
+        final int solution = (int) ((Math.random() * niveau) + 1);
+        System.out.println(solution);
+        System.out.println("C'est parti, trouve le nombre entre 1 et " + niveau);
+        while (!clavier.hasNextInt()) {
+            System.out.println("Entrez un entier ");
+            clavier.next();
+        }
+        int essai = clavier.nextInt();
+        while (essai < 1 || essai > niveau) {
+            System.out.println("Entre 1 et " + niveau + " j'ai dit");
+            while (!clavier.hasNextInt()) {
+                System.out.println("Entrez un entier ");
+                clavier.next();
+            }
+            essai = clavier.nextInt();
+        }
+        while (essai != solution) {
+            if (essai < solution) {
+                System.out.println("Reessaye c'est plus grand");
+            } else {
+                System.out.println("Ressaye c'est plus petit");
+            }
+
+            while (!clavier.hasNextInt()) {
+                System.out.println("Entrez un entier ");
+                clavier.next();
+            }
+            essai = clavier.nextInt();
+            while (essai < 1 || essai > niveau) {
+                System.out.println("Entre 1 et " + niveau + " j'ai dit");
+                while (!clavier.hasNextInt()) {
+                    System.out.println("Entrez un entier ");
+                    clavier.next();
+                }
+                essai = clavier.nextInt();
+            }
+        }
+        System.out.println("BRAVOOOOO CHAMPION");
+    }
+}
