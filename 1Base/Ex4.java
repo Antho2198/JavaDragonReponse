da4a7f9e2ed8cc2a0d3767456ddaf30fd61b6e78
Antho2198 <anthony.dragone17@gmail.com>
2021-07-03 17:05:25 +0200

    4ème push

---
+++ 1Base/Ex4.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Ex4 {
+    public static void main(String[] args) {
+        System.out.println("Entrez un nombre");
+        Scanner clavier = new Scanner(System.in);
+        int nombre = clavier.nextInt();
+        while (nombre != 0) {
+            System.out.println("Entre un autre nombre");
+            nombre = clavier.nextInt();
+        }
+        if (nombre == 0) {
+            System.out.println("Au revoir");
+        }
+    }
+}
